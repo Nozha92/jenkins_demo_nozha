a7b494a12404363db981a86bb9f552f53e5b28ec
Nozha92 <skillsoft666@gmail.com>
2025-11-16 12:13:06 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,6 @@
-print("Hello Nozha, script Python exécuté depuis GitHub ! 🎉")
+# script.py
+
+print("Tests Python OK ✅")
+
+with open("result_github.txt", "w") as f:
+    f.write("Hello Nozha, ton pipeline marche avec GitHub et Jenkins !\n")
